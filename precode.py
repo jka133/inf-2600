41506ddf2b38032d4503764f4077bf65e2c5c05a
jka133 <111968552+jka133@users.noreply.github.com>
2024-05-11 09:13:24 +0200

    made the model finally

--- precode.py
+++ precode.py
@@ -7,7 +7,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 from pgmpy.models import BayesianNetwork
-from pgmpy.factors.discrete import TabularCPD
+from pgmpy.factors.discrete.CPD import TabularCPD
 
 # Import data, make a copy of the original
 df0 = pd.read_csv('seattle-weather.csv')
@@ -57,7 +57,7 @@ labels = {
 } 
 
 # Create bounds for continuous labels
-prec_mean = df0['precipitation'].mean() # I made the decision to make percipitation threefold
+prec_mean = df0['precipitation'].mean()
 prec_stdv = df0['precipitation'].std()
 
 temp_max_mean = df0['temp_max'].mean()
@@ -96,11 +96,10 @@ print(df)
 # Define the hierarchy
 weather_model = BayesianNetwork([
     ('weather', 'precipitation'),
-    ('precipitation', 'temp_max'),
     ('weather', 'wind'),
+    ('precipitation', 'temp_max'),
     ('wind', 'temp_min')
 ])
-print('All good')
 
 # And, the states for each variables
 states = {
@@ -117,6 +116,7 @@ states = {
 # Weather does not have any parents so all we need are the marginal probabilities of observing each weather type
 weather_marginal = (df['weather'].value_counts()/len(df['weather'])).round(3)
 weather_marginal = np.array([[value] for value in weather_marginal])
+print(weather_marginal) # Seems right with the info from assignment
 
 
 # Joint Propabilities
@@ -127,38 +127,84 @@ var_dict = {'weather': ['precipitation', 'wind'],
            }
 
 
-# Create conditional distributions and store results in a list
+""" # Create conditional distributions and store results in a list
 cpd_lst = []
 for key, value in var_dict.items():
    length = len(value)
    for i in range(length):
-       value_given_key = df.groupby(key)[value[i]].value_counts(
-                                                   normalize=True
-                                                   ).sort_index()
+       value_given_key = df.groupby(key)[value[i]].value_counts(normalize=True).sort_index()
        cpd = value_given_key.unstack(fill_value=0).to_numpy().T
-       cpd_lst.append(cpd)
+       cpd_lst.append(cpd)   """
 """ End code from TA """
+
+
+""" Code from TA gave the wrong shapes of the CPDs """
+# Create conditional distributions and store results in a list
+cpd_lst = []
+for key, value in var_dict.items():
+    for child in value:
+        # Group by the key and count occurrences of each state in the child, normalized
+        grouped = df.groupby(key)[child].value_counts(normalize=True)
+        # Unstack and reindex with all possible states to ensure complete shape
+        cpd = grouped.unstack(fill_value=0).reindex(columns=states[child], index=states[key], fill_value=0).to_numpy().T
+        cpd_lst.append(cpd)
+# Print the shapes of the four CPDs
+""" for cpd_array in cpd_lst:
+    print(cpd_array.shape) """
+
 # Note that we get 3 Nan vslues in the above conditional distributions. This is because one of the type of precipitation (low) did not contain any relation with temp_max.
 # Therefore, normalization, does not produce the intended result.
 # To mitigate this, we replace Nan with the equal probability within the three values, i.e., 0.33
+
 cpd_lst[2][:,0] = .33
 print(cpd_lst)
-exit()
+# Print the shapes of the four CPDs
+""" for cpd_array in cpd_lst:
+    print(cpd_array) """
 
 # https://pgmpy.org/models/bayesiannetwork.html
 # Zoom out and use meny bar to find Exact Inference and Approximate Inference for the next tasks
 
-cpd_lst
+#cpd_lst
 
 # Creating tabular conditional probability distribution
 
+# CPD for precipitation given weather
+weather_cpd = TabularCPD(variable='weather', variable_card=5,
+                         values=weather_marginal, # has no evidence
+                         state_names={'weather': ['drizzle', 'rain', 'sun', 'snow', 'fog']})
 
+precipitation_cpd = TabularCPD(variable='precipitation', variable_card=3,
+                               values=cpd_lst[0],
+                               evidence=['weather'], evidence_card=[5],
+                               state_names={'precipitation': ['low', 'mid', 'high'],
+                                            'weather': ['drizzle', 'rain', 'sun', 'snow', 'fog']})
+
+temp_max_cpd = TabularCPD(variable='temp_max', variable_card=3,
+                      values=cpd_lst[2],
+                      evidence=['precipitation'], evidence_card=[3],
+                      state_names={'temp_max': ['low', 'mid', 'high'],
+                                    'precipitation': ['low', 'mid', 'high']})
+
+wind_cpd = TabularCPD(variable='wind', variable_card=3,
+                      values=cpd_lst[1],
+                      evidence=['weather'], evidence_card=[5],
+                      state_names={'wind': ['low', 'mid', 'high'],
+                                    'weather': ['drizzle', 'rain', 'sun', 'snow', 'fog']})
+
+temp_min_cpd = TabularCPD(variable='temp_min', variable_card=3,
+                      values=cpd_lst[2],
+                      evidence=['wind'], evidence_card=[3],
+                      state_names={'temp_min': ['low', 'mid', 'high'],
+                                    'wind': ['low', 'mid', 'high']})
 
 # Add CPDs and factors to the model
 
+weather_model.add_cpds(weather_cpd, precipitation_cpd, wind_cpd, temp_max_cpd, temp_min_cpd)
+print(weather_model)
 
 # Check if model is consistent
-
+print(weather_model.check_model())
 
 # Viewing nodes of the model
 weather_model.nodes()
@@ -171,6 +217,8 @@ print(weather_cpd)
 
 # Print the probability table of the wind node
 print(wind_cpd)
+
+exit()
 
 # Independcies in the model
 
